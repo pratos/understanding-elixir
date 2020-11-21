20bb87f409cf3e8412667c85d15d8523693d5fc0
Prathamesh Sarang <prathamesh@difference-engine.ai>
2020-11-21 09:34:10 +0530

    feat(project): add new project

---
+++ recurring/lib/recurring.ex
@@ -0,0 +1,18 @@
+defmodule Recurring do
+  @moduledoc """
+  Documentation for `Recurring`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recurring.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ recurring/test/recurring_test.exs
@@ -0,0 +1,8 @@
+defmodule RecurringTest do
+  use ExUnit.Case
+  doctest Recurring
+
+  test "greets the world" do
+    assert Recurring.hello() == :world
+  end
+end
